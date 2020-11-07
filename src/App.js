8f8d7c94670504255267cbb4e7ecd85e97bae117
Andre Alef Degaspari Moura <alef.moura@verithus.com.br>
2020-11-06 23:19:09 -0300

    Adicionando regras

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [repositories])
   async function handleAddRepository() {
-    // TODO
+    
+    const response =  await api.post('repositories', { 
+      title: "NodeJs",
+      url: "github.com",
+      techs:[ "Node.js", "React" ]
+    })
+
+    const repository = response.data
+    console.log(repository)
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+   await api.delete('repositories/' + id )
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      
+        {repositories.map(repository =>{
+          return( 
+            <>
+            <li key={repository.id}>{repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+          </li>
+          </>
+          )
+          })}
+      
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
